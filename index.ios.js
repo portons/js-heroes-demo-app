5ff6f4b552cc710fb192eac0719736994ab7adab
Vova Porton <vova@500tech.com>
2017-06-03 13:22:40 +0300

    Adjusted the master version to the demo

--- index.ios.js
+++ index.ios.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { AppRegistry } from 'react-native';
+
+import store from 'core/store/store';
 
 import JsHeroes from 'native-components/js-heroes';
 
 const App = () => (
-	<JsHeroes/>
+	<Provider store={ store }>
+		<JsHeroes/>
+	</Provider>
 );
 
 AppRegistry.registerComponent(
--- native/components/js-heroes.js
+++ native/components/js-heroes.js
@@ -1,31 +1,45 @@
 import React from 'react';
-import { StatusBar, Text, View, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+import { StatusBar, View, StyleSheet } from 'react-native';
 import Anime from 'react-native-anime';
 
-//import Button from 'native-components/demo/button';
-//import TalksList from 'native-components/demo/talks-list';
+import { fetchTalks, selectTalk } from 'core/actions/talks';
+
+import { talksSelector, selectedTalkSelector } from 'core/selectors/talks';
+
+import Button from 'native-components/demo/button';
+import TalksList from 'native-components/demo/talks-list';
 
 class JsHeroes extends React.Component {
 	render() {
 		return (
-			<View style={ styles.container }
-						ref={ ref => this.title = ref }>
-				<Text style={{ fontSize: 30 }}>EMPTY</Text>
+			<View style={ styles.container }>
+				<Anime.Text style={ styles.title }
+										ref={ ref => this.title = ref }>HELLO JS HEROES!</Anime.Text>
+
+				{
+					this.props.talks
+						? <TalksList talks={ this.props.talks }
+												 selectTalk={ this.props.selectTalk }
+												 selectedTalk={ this.props.selectedTalk }/>
+						: <Button text="FETCH TALKS"
+											onPress={ this.props.fetchTalks }/>
+				}
 			</View>
 		);
 	}
 
-	//componentWillMount() {
-	//	StatusBar.setBarStyle('light-content', true);
-	//}
-	//
-	//componentDidUpdate(prevProps) {
-	//	if (this.props.talks && !prevProps.talks) {
-	//		this.title
-	//			.color('#228dcb', { duration: 2000 })
-	//			.start();
-	//	}
-	//}
+	componentWillMount() {
+		StatusBar.setBarStyle('light-content', true);
+	}
+
+	componentDidUpdate(prevProps) {
+		if (this.props.talks && !prevProps.talks) {
+			this.title
+				.color('#228dcb', { duration: 2000 })
+				.start();
+		}
+	}
 }
 
 const styles = StyleSheet.create({
@@ -33,7 +47,8 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center',
 		flex: 1,
-		paddingTop: 30
+		paddingTop: 30,
+		backgroundColor: '#222'
 	},
 	title: {
 		fontSize: 40,
@@ -42,4 +57,9 @@ const styles = StyleSheet.create({
 	}
 });
 
-export default JsHeroes;
+const mapStateToProps = (state) => ({
+	talks: talksSelector(state),
+	selectedTalk: selectedTalkSelector(state)
+});
+
+export default connect(mapStateToProps, { fetchTalks, selectTalk })(JsHeroes);
